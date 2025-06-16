bab8ffbb9ae9312740c3cb6473c3c9684fe5c310
Abhijeet Giram <abhijeetgrm@gmail.com>
2025-06-16 20:10:35 +0530

    Updated UI for light dark theme

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -20,10 +20,17 @@ const ProductListPage: React.FC = () => {
   const { theme } = useTheme();
   const [products] = useState<Product[]>(mockProducts);
 
+  const containerClass =
+    theme === "dark" ? styles.darkContainer : styles.container;
+  const themeClass = theme === "dark" ? styles.darkTheme : styles.theme;
+  const headingClass = theme === "dark" ? styles.darkHeading : styles.heading;
+
   return (
-    <div className={styles.container}>
-      This is a product list in {theme} mode.
-      <h1 className={styles.heading}>Product List</h1>
+    <div className={containerClass}>
+      <p className={themeClass}>
+        This is a product list in <b>{theme}</b> mode.
+      </p>
+      <h1 className={headingClass}>Product List</h1>
       <div className={styles.grid}>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
